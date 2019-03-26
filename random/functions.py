576993412cd1fc9dd8c9b987577f5fe4a74ceac5
Ignacio Cifuentes <ignacio.cifuentes948@gmail.com>
2019-03-26 01:22:58 -0300

    update

--- random/functions.py
+++ random/functions.py
@@ -5,7 +5,7 @@ def generate_dynamic_file(number_of_particles, area_length, name):
     with open(name, 'w') as f:
         f.write('0\n')
         for x in range(0, number_of_particles):
-            randomAngle = random.uniform(-pi, pi)
+            randomAngle = random.uniform()*2*pi
             f.write('{} {} {}\n'.format(random.uniform(0, area_length), random.uniform(0, area_length), randomAngle))
 
 def generate_files(number_of_particles, area_length):
