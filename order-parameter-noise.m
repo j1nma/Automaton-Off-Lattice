d5b9049cbf5b0fce1ea9f4d4db09ac26a1b04fcd
j1nma <j1nma@users.noreply.github.com>
2019-03-29 02:29:03 -0300

    order-parameter-noise corrections

--- order-parameter-noise.m
+++ order-parameter-noise.m
@@ -1,20 +1,21 @@
 #! /bin/octave -qf
 
 defaultVelocity = 0.03;
-duration=1000;
+duration=2000;
 
 rc=1;
-simulation_data_values={40, 3.1, 's', 3, 'r'; 100, 5, '+', 4, 'b'; 400, 10, 'x', 9, 'g'};
+simulation_data_values={40, 3.1, 's', 3, 'r'; 100, 5, 'd', 4, 'b'; 400, 10, 'x', 9, 'g'};
+simulation_data_values_p_2={50, 5, 's', 4, 'r'; 100, 7.07, 'd', 7, 'b'; 500, 15.81, 'x', 15, 'g'};
 
-etha_values=0:0.25:5;
+etha_values=0:0.5:5;
 
 hold on;
 
-for i=1:rows(simulation_data_values)
-       N=simulation_data_values{i,1};
-       L=simulation_data_values{i,2};
-       M=simulation_data_values{i,4};
-       marker=simulation_data_values{i,3};
+for i=1:rows(simulation_data_values_p_2)
+       N=simulation_data_values_p_2{i,1};
+       L=simulation_data_values_p_2{i,2};
+       M=simulation_data_values_p_2{i,4};
+       marker=simulation_data_values_p_2{i,3};
 
        outputFileName = sprintf("./output/duration=%d/N=%d-L=%d-M=%d.txt", duration, N, L, M);
        outputFile = fopen(outputFileName, 'r');
@@ -39,11 +40,11 @@ for i=1:rows(simulation_data_values)
         plot_index += 1;
       end
 
-      fmt = sprintf(".%s", simulation_data_values{i,5});
+      fmt = sprintf(".%s", simulation_data_values_p_2{i,5});
       plot = errorbar(etha_values, va_plot_values, std_plot_values, fmt);
       set(plot, "linestyle", "none");
       set(plot, "marker", marker);
-      %plot(etha_values, va_plot_values, "marker", marker, "linestyle", "none", "color", simulation_data_values{i,5});
+      %plot(etha_values, va_plot_values, "marker", marker, "linestyle", "none", "color", simulation_data_values_p_2{i,5});
       fclose(outputFile);
 endfor;
 
